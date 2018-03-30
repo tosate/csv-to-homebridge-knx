405f43367f22709e9cbe5fe25a227cb2e8840f98
Thomas <tosate@gmail.com>
2018-03-30 14:25:15 +0200

    Outlets behave like Lightbulbs.

--- src/main/java/de/devtom/java/homekit/OutletService.java
+++ src/main/java/de/devtom/java/homekit/OutletService.java
@@ -18,17 +18,11 @@ public class OutletService extends AbstractService {
 	protected void initialize(KnxCsvRecord record) {
 		Characteristics onCharacteristics = new Characteristics();
 		onCharacteristics.setType(CharacteristicsType.ON);
+		onCharacteristics.getListen().add(record.getGroupAddress2());
 		onCharacteristics.getSet().add(record.getGroupAddress1());
 		onCharacteristics.setDpt(OutletService.KNX_DPT1);
 
 		this.getCharacteristics().add(onCharacteristics);
-		
-		Characteristics outletInUseCharacteristics = new Characteristics();
-		outletInUseCharacteristics.setType(CharacteristicsType.OUTLETINUSE);
-		outletInUseCharacteristics.getListen().add(record.getGroupAddress2());
-		outletInUseCharacteristics.setDpt(OutletService.KNX_DPT1);
-		
-		this.getCharacteristics().add(outletInUseCharacteristics);
 	}
 
 }
